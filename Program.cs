36d670c17497106924011e2930094e887e98048d
Oskar Lundqvist <oskar@Oskars-MacBook-Air.local>
2016-10-31 16:59:28 +0100

    Created project in MonoDevelop

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//
+// Code by Oskar Lundqvist / Aborvinsch 2016
+//
+// ------------------------------------------------------------------------------
+using System;
+
+namespace Emelie
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
